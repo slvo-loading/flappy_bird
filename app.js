122ca45474cf912add72a5d9c238a8b5f7ef8f16
slvo-loading <sydneyvo098@gmail.com>
2024-09-17 01:20:58 -0700

    basically done just gotta tweek the ending of the game

--- app.js
+++ app.js
@@ -30,15 +30,77 @@ function preload() {
   this.load.image('road', 'assets/road.png');
 }
 
+let bird;
+let hasLanded = false;
+let curser;
+let hasBumped = false;
+let isGameStarted = false;
+let messageToPlayer;
+
 function create() {
   const background = this.add.image(0, 0, 'background')
     .setOrigin(0, 0)
     .setDisplaySize(my_width, my_height - 100);
 
     const roads = this.physics.add.staticGroup();
+
+    const topColumns = this.physics.add.staticGroup({
+        key: 'column',
+        repeat: 1,
+        setXY: { x: 300, y: 50, stepX: 500 }
+    });
+
+    const bottomColumns = this.physics.add.staticGroup({
+        key:'column',
+        repeat: 1,
+        setXY: { x: 550, y: 400, stepX: 500 }
+    })
+
     const road = roads.create(400, 568, 'road')
         .setScale(2)
         .refreshBody();
+
+    bird = this.physics.add.sprite(0, 50, 'bird').setScale(2);
+    bird.setBounce(0.2);//bounces slightly when it hits something
+    bird.setCollideWorldBounds(true);
+
+    this.physics.add.overlap(bird,road, () => hasLanded = true, null, this);
+    this.physics.add.collider(bird, road);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.overlap(bird, topColumns, () => hasBumped = true, null, this);
+    this.physics.add.overlap(bird, bottomColumns, () => hasBumped = true, null, this);
+    this.physics.add.collider(bird, topColumns);
+    this.physics.add.collider(bird, bottomColumns);
+
+    messageToPlayer = this.add.text(0, 0, 'press SPACE to start', { fontSize: '32px', fill: '#000' });
+    Phaser.Display.Align.In.BottomCenter(messageToPlayer, background, 0, 50);
 }
 
-function update() {}
+function update() {
+    if(cursors.space.isDown && !isGameStarted) {
+        isGameStarted = true;
+        messageToPlayer.text = 'press UP key to fly and avoid the columns';
+    }
+
+    if (cursors.up.isDown && !hasLanded && !hasBumped) {
+        bird.setVelocityY(-160);
+    }
+
+    if (!isGameStarted) {
+        bird.setVelocityY(-160);
+      }
+
+    if(!hasLanded && !hasBumped) {
+        bird.body.velocity.x = 50;
+    } 
+    
+    if (hasLanded || hasBumped || !isGameStarted) {
+        bird.body.velocity.x = 0;
+    }
+
+    if(hasLanded || hasBumped) {
+        messageToPlayer.text = 'Game Over! press F5 to restart';
+    }
+}
